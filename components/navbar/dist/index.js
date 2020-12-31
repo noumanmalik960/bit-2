ebd5e7e6a4cef2f1e66569919ccc6ffa4df452ef
Nouman Javed <noumanmalik960@gmail.com>
2020-12-31 20:20:56 +0500

    final commit

---
+++ components/navbar/dist/index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var NavBar = function NavBar() {
+  return /*#__PURE__*/_react.default.createElement("div", null, /*#__PURE__*/_react.default.createElement("h1", null, "This is NavBar component. (UPDATED)"));
+};
+
+var _default = NavBar;
+exports.default = _default;
+
+//# sourceMappingURL=index.jsx.map
